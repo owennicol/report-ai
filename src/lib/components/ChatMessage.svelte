e305c0a45cf9df1f0abf5f489fb14149a778176d
Owen Nicol <owennicol@users.noreply.github.com>
2023-12-30 20:04:32 +0000

    first commit

---
+++ src/lib/components/ChatMessage.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import type { ChatCompletionRequestMessageRoleEnum } from 'openai'
+	import SvelteMarkdown from 'svelte-markdown'
+	export let type: ChatCompletionRequestMessageRoleEnum
+	export let message: string
+</script>
+
+<div class="chat {type === 'user' ? 'chat-end' : 'chat-start'} justify-end">
+	<div class="chat-image avatar">
+		<div class="w-10 rounded-full">
+			<img
+				src="https://ui-avatars.com/api/?name={type === 'user' ? 'Me' : 'RB'}"
+				alt="{type} avatar"
+			/>
+		</div>
+	</div>
+	<div class="chat-header">
+		{type === 'user' ? 'Me' : 'ReportBot'}
+	</div>
+	<div class="chat-bubble {type === 'user' ? 'chat-bubble-info' : 'chat-bubble'}">
+		<SvelteMarkdown source={message} />
+		<!-- {message} -->
+	</div>
+</div>
---
+++ src/routes/page.test.svelte
@@ -0,0 +1,95 @@
+import { handleSubmit } from './index'
+
+describe('handleSubmit', () => {
+  it('should set the loading state to true', () => {
+    const state = {
+      loading: false,
+      chatMessages: [],
+      query: '',
+      answer: ''
+    }
+
+    handleSubmit(state)
+
+    expect(state.loading).toBe(true)
+  })
+
+  it('should add the user\'s query to the chatMessages array', () => {
+    const state = {
+      loading: false,
+      chatMessages: [],
+      query: 'Hello',
+      answer: ''
+    }
+
+    handleSubmit(state)
+
+    expect(state.chatMessages).toEqual([{ role: 'user', content: 'Hello' }])
+  })
+
+  it('should create a new SSE event source and send the chatMessages array to the server', () => {
+    const state = {
+      loading: false,
+      chatMessages: [],
+      query: '',
+      answer: ''
+    }
+
+    handleSubmit(state)
+
+    expect(state.chatMessages).toEqual([{ role: 'user', content: '' }])
+  })
+
+  it('should set the query state to an empty string', () => {
+    const state = {
+      loading: false,
+      chatMessages: [],
+      query: 'Hello',
+      answer: ''
+    }
+
+    handleSubmit(state)
+
+    expect(state.query).toBe('')
+  })
+
+  it('should add an event listener to the event source for the \'error\' event', () => {
+    const state = {
+      loading: false,
+      chatMessages: [],
+      query: '',
+      answer: ''
+    }
+
+    handleSubmit(state)
+
+    expect(state.chatMessages).toEqual([{ role: 'user', content: '' }])
+  })
+
+  it('should add an event listener to the event source for the \'message\' event', () => {
+    const state = {
+      loading: false,
+      chatMessages: [],
+      query: '',
+      answer: ''
+    }
+
+    handleSubmit(state)
+
+    expect(state.chatMessages).toEqual([{ role: 'user', content: '' }])
+  })
+
+  it('should call the scrollToBottom function', () => {
+    const state = {
+      loading: false,
+      chatMessages: [],
+      query: '',
+      answer: ''
+    }
+
+    handleSubmit(state)
+
+    expect(state.chatMessages).toEqual([{ role: 'user', content: '' }])
+  })
+})
+
